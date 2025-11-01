161bb63283b25946c541dd86899851b9ae1657d9
itsuki <itsuki.enjyoy@gmail.com>
2025-11-01 11:38:45 +0900

    Initial Commit

---
+++ GroupActivityRemoteMediaCoordination/GroupActivityRemoteMediaCoordinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupActivityRemoteMediaCoordinationApp.swift
+//  GroupActivityRemoteMediaCoordination
+//
+//  Created by Itsuki on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct GroupActivityRemoteMediaCoordinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
